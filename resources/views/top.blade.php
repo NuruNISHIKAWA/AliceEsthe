9d6ae24068e248f65030cd182d27344326f97f86
Nishikawa <knishikawa0120@yahoo.co.jp>
2022-10-26 23:35:49 +0900

    message

---
+++ resources/views/top.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+  <title>toppage</title>
+</head>
+<body>
+
+<header>
+    <div>
+            名古屋栄メンズエステ【nurunuru（ぬるぬる）】
+        </div>
+        <div>
+            <img src="img/download.png" alt="">
+        </div>
+  </header>
+
+<!---
+  <header class="header flex__item">
+    <div class="header__txt">
+            名古屋栄メンズエステ【nurunuru（ぬるぬる）】
+        </div>
+        <div class="header__logo">
+            <img src="img/download.png" alt="">
+        </div>
+  </header>
+  !--->
+</body>
+</html>
