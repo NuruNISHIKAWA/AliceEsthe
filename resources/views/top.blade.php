63930d4fbc9c992c1d78a8c53abd41b888d435e9
Nishikawa <knishikawa0120@yahoo.co.jp>
2022-10-27 03:10:11 +0900

    message

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -10,25 +10,164 @@
 </head>
 <body>
 
-<header>
-    <div>
+<header class="header">
+        <p class="htext">MENU</p>
+</header>
+
+<div>
+    <div class="tophome">
             名古屋栄メンズエステ【nurunuru（ぬるぬる）】
         </div>
         <div>
             <img src="img/IMG_1287.PNG" alt="">
         </div>
-        <p>
+        <p class="home">
           貴方と私の二人だけの時間<br><br>
           ようこそ楽園へ
         </p>
-        <div>
+        <div class="topgirl">
             <img src="img/IMG_1286.JPG" alt="">
         </div>
 
-        <div>
+        <div class="tell">
             <img src="img/download.png" alt="">
         </div>
-  </header>
+</div>
+
+  <div id="back">
+    <div>
+    <p class="common__ttl">Realtime</p>
+    <div class="tweet">
+    <a class="twitter-timeline" href="https://twitter.com/cancan_2022?ref_src=twsrc%5Etfw" data-width="100%" data-tweet-limit="2" data-chrome="noheader">Tweets by cancan_2022</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+</div>
+
+<div class="space">
+    <p class="common__ttl">Pickup Theraphist</p>
+    <div class="flex__item">
+      <a class="item" href="">
+        <img src="img/IMG_1286.JPG" alt="">
+</a>
+<a class="item" href="">
+        <img src="img/IMG_1286.JPG" alt="">
+</a>
+<a class="item" href="">
+        <img src="img/IMG_1286.JPG" alt="">
+</a>
+</div>
+<div class="flex__item">
+      <a class="item" href="">
+        <img src="img/IMG_1286.JPG" alt="">
+</a>
+<a class="item" href="">
+        <img src="img/IMG_1286.JPG" alt="">
+</a>
+<a class="item" href="">
+        <img src="img/IMG_1286.JPG" alt="">
+</a>
+</div>
+</div>
+    </div>
+
+    <div class="space">
+      <p class="common__ttl">Event Information</p>
+      <a class="topgirl2" href="">
+        <img src="img/IMG_1291.PNG" alt="">
+</a>
+</div>
+
+
+
+
+<div class="space">
+    <p class="common__ttl">Today's Theraphist</p>
+    <div class="flex__item">
+      <div class="today">
+      <a class="todayitem" href="">
+        <img src="img/IMG_1286.JPG" alt="">
+</a>
+<h2 class="todaytext">ちーず</h3>
+<h2 class="todaytext2">12:00〜21:00</h3>
+</div>
+
+<div class="today">
+      <a class="todayitem" href="">
+        <img src="img/IMG_1286.JPG" alt="">
+</a>
+<h2 class="todaytext">じじ</h3>
+<h2 class="todaytext2">12:00〜21:00</h3>
+</div>
+
+
+</div>
+
+<div class="flex__item">
+      <div class="today">
+      <a class="todayitem" href="">
+        <img src="img/IMG_1286.JPG" alt="">
+</a>
+<h2 class="todaytext">ぐらん</h3>
+<h2 class="todaytext2">12:00〜21:00</h3>
+</div>
+
+<div class="today">
+      <a class="todayitem" href="">
+        <img src="img/IMG_1286.JPG" alt="">
+</a>
+<h2 class="todaytext">らむ</h3>
+<h2 class="todaytext2">12:00〜21:00</h3>
+</div>
+
+
+</div>
+
+<div class="flex__item">
+      <div class="today">
+      <a class="todayitem" href="">
+        <img src="img/IMG_1286.JPG" alt="">
+</a>
+<h2 class="todaytext">なの</h3>
+<h2 class="todaytext2">12:00〜21:00</h3>
+</div>
+
+</div>
+
+</div>
+
+<div id="back2">
+<div class="space">
+      <p class="common__ttl">New Theraphist</p>
+      <a class="topgirl2" href="">
+        <img src="img/IMG_1292.PNG" alt="">
+</a>
+<a class="topgirl2" href="">
+        <img src="img/IMG_1292.PNG" alt="">
+</a>
+</div>
+
+<div class="space">
+      <p class="common__ttl">News</p>
+      <a class="topgirl2" href="">
+        <img src="img/IMG_1293.PNG" alt="">
+</a>
+
+</div>
+
+<div class="space">
+      <p class="common__ttl">Link</p>
+      <a class="topgirl2" href="">
+        <img src="img/IMG_1294.PNG" alt="">
+</a>
+
+</div>
+
+</div>
+</div>
+
+<footer class="footer">
+        <p>Copyright © 名古屋栄メンズエステ【nurunuru（ぬるぬる）】 All Rights Reserved.</p>
+    </footer>
+
+
 
 <!---
   <header class="header flex__item">
